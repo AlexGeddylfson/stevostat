952c0d5083a29e7f3f1a44714a57babf9eb38c71
AlexGeddylfson <Alexstephenandrewrobinson@gmail.com>
2023-05-12 19:50:41 -0400

    Major overhaul
    Added clock for time keeping
    Added auto data refresh for temp and humidity sensor
    Modified UI

--- thermostat.py
+++ thermostat.py
@@ -8,7 +8,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
 
 # Set up the DHT sensor
-DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
 # Create Flask app
@@ -17,7 +17,17 @@ app = Flask(__name__)
 # Function to read temperature and humidity
 def read_temperature_humidity():
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    return humidity, temperature
+    return int(humidity), temperature
+
+# Function to convert temperature from Celsius to Fahrenheit
+def celsius_to_fahrenheit(temperature_celsius):
+    temperature_fahrenheit = round((temperature_celsius * 9/5) + 32, 1)
+    return temperature_fahrenheit
+
+# Function to round temperature to the nearest 0.5
+def round_temperature(temperature):
+    rounded_temperature = round(temperature * 2) / 2
+    return rounded_temperature
 
 # Function to control the relay
 def set_relay_state(state):
@@ -26,20 +36,30 @@ def set_relay_state(state):
 # Home page route
 @app.route('/')
 def home():
-    humidity, temperature = read_temperature_humidity()
-    return render_template('index.html', temperature=temperature, humidity=humidity)
+    return render_template('index.html')
+
+# API endpoint to retrieve temperature and humidity data
+@app.route('/data', methods=['GET'])
+def get_data():
+    humidity, temperature_celsius = read_temperature_humidity()
+    temperature_fahrenheit = celsius_to_fahrenheit(temperature_celsius)
+    rounded_temperature = round_temperature(temperature_fahrenheit)
+    data = {
+        'temperature': rounded_temperature,
+        'humidity': humidity
+    }
+    return data
 
 # Thermostat control route
 @app.route('/thermostat', methods=['POST'])
 def thermostat():
-    target_temperature = float(request.form['temperature'])
+    target_temperature_fahrenheit = float(request.form['temperature'])
+    target_temperature_celsius = (target_temperature_fahrenheit - 32) * 5/9
     # Implement your thermostat logic here
-    # You can compare target_temperature with the current temperature and control the relay accordingly
+    # You can compare target_temperature_celsius with the current temperature and control the relay accordingly
     set_relay_state(GPIO.HIGH)
-    return "Thermostat set to {}°C".format(target_temperature)
+    return "Thermostat set to {}°F".format(target_temperature_fahrenheit)
 
-if __name__ == '__main__':
-    app.run()
 
 # Clean up GPIO
 GPIO.cleanup()
