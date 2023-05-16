828c6f62869259cbacdf1f76f99f778c91564ae1
AlexGeddylfson <Alexstephenandrewrobinson@gmail.com>
2023-05-15 21:19:45 -0400

    Update thermostat.py
    Added a bunch of logic for hvac relay controller

--- thermostat.py
+++ thermostat.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 import Adafruit_DHT
-from flask import Flask, render_template, request
+import time
+from flask import Flask, render_template, request, jsonify
 
 # Set up GPIO pins for the relay module
-RELAY_PIN = 21
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RELAY_PIN, GPIO.OUT)
+RELAY_PINS = [27, 22, 23, 24]
+GPIO.setmode(GPIO.BCM)  # Set the GPIO pin numbering mode to BCM
+for pin in RELAY_PINS:
+    GPIO.setup(pin, GPIO.OUT)
 
 # Set up the DHT sensor
 DHT_SENSOR = Adafruit_DHT.DHT22
@@ -24,14 +26,30 @@ def celsius_to_fahrenheit(temperature_celsius):
     temperature_fahrenheit = round((temperature_celsius * 9/5) + 32, 1)
     return temperature_fahrenheit
 
-# Function to round temperature to the nearest 0.5
+# Function to round temperature to the nearest 0.5 degrees
 def round_temperature(temperature):
     rounded_temperature = round(temperature * 2) / 2
     return rounded_temperature
 
 # Function to control the relay
-def set_relay_state(state):
-    GPIO.output(RELAY_PIN, state)
+def set_relay_state(relay_number, state):
+    GPIO.output(RELAY_PINS[relay_number], state)
+
+# Function to check and control the thermostat
+def check_thermostat(target_temperature_celsius):
+    while True:
+        humidity, current_temperature = read_temperature_humidity()
+        if current_temperature < target_temperature_celsius:
+            set_relay_state(0, GPIO.HIGH)
+            set_relay_state(1, GPIO.HIGH)
+            set_relay_state(2, GPIO.HIGH)
+            set_relay_state(3, GPIO.HIGH)
+        else:
+            set_relay_state(0, GPIO.LOW)
+            set_relay_state(1, GPIO.LOW)
+            set_relay_state(2, GPIO.LOW)
+            set_relay_state(3, GPIO.LOW)
+        time.sleep(5)  # Wait for 5 seconds before checking the temperature again
 
 # Home page route
 @app.route('/')
@@ -48,18 +66,16 @@ def get_data():
         'temperature': rounded_temperature,
         'humidity': humidity
     }
-    return data
+    return jsonify(data)
 
 # Thermostat control route
 @app.route('/thermostat', methods=['POST'])
 def thermostat():
     target_temperature_fahrenheit = float(request.form['temperature'])
     target_temperature_celsius = (target_temperature_fahrenheit - 32) * 5/9
-    # Implement your thermostat logic here
-    # You can compare target_temperature_celsius with the current temperature and control the relay accordingly
-    set_relay_state(GPIO.HIGH)
+    print("Target temperature set to:", target_temperature_celsius)
+    check_thermostat(target_temperature_celsius)
     return "Thermostat set to {}°F".format(target_temperature_fahrenheit)
 
-
-# Clean up GPIO
-GPIO.cleanup()
+if __name__ == '__main__':
+    app.run(debug=True)
